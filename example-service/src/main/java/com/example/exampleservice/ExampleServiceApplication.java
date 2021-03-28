3b9c8a8ffd921ada5441c9742ceee5d8643561e3
iranneto <iranmacedo.bneto@gmail.com>
2021-03-28 16:48:58 -0300

    SB Admin server and client ok (not by docker)

--- example-service/src/main/java/com/example/exampleservice/ExampleServiceApplication.java
+++ example-service/src/main/java/com/example/exampleservice/ExampleServiceApplication.java
@@ -2,6 +2,9 @@ package com.example.exampleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class ExampleServiceApplication {
--- sb-admin/src/main/java/com/sb/admin/sbadmin/SbAdminApplication.java
+++ sb-admin/src/main/java/com/sb/admin/sbadmin/SbAdminApplication.java
@@ -4,11 +4,11 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 @EnableAdminServer
+@SpringBootApplication
 public class SbAdminApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SbAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SbAdminApplication.class, args);
+    }
 
 }
